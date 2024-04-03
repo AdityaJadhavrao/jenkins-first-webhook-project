1611e7ecf10fb0bed2c474aa286501925f3fcda9
EC2 Default User <ec2-user@ip-172-31-36-53.ap-southeast-2.compute.internal>
2024-04-03 16:52:48 +0000

    Udoated second time hello.java

--- Hello.java
+++ Hello.java
@@ -1,6 +1,7 @@
 public class Hello {
 
 public static void main(String args[]){
+System.out.println("This is a new  hello World");
 System.out.println("hello world");
 System.out.println("Hello from Java");
 }
